56dd04d5ed1c2c47264cdef73692fe3cbbe99421
RUSSEN BISSEMO <russenbissemo@gmail.com>
2023-04-27 00:31:56 +0200

    keep going buddy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     FormsModule,
+    HttpClientModule
  
   ],
   providers: [],
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,48 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import {IProduct} from './product';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IProduct } from './product';
 import { ProductService } from './product.service';
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit , OnDestroy {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
   showImage: boolean = false;
+  erroMessage: string = '';
+  sub!: Subscription;
 
   private _listFilter: string = '';
   get listFilter(): string {
     return this._listFilter;
   }
   set listFilter(value: string) {
-    this._listFilter = value ; 
-    console.log('In setter:' , value);
+    this._listFilter = value;
+    console.log('In setter:', value);
     this.filteredProducts = this.performFilter(value);
   }
- filteredProducts : IProduct[] = [];
+  filteredProducts: IProduct[] = [];
 
   products: IProduct[] = [];
 
-  constructor(private  productService : ProductService) {
-
-  }
+  constructor(private productService: ProductService) {}
   performFilter(filterBy: string): IProduct[] {
-    filterBy = filterBy.toLowerCase(); 
-    return this.products.filter((product:IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
   }
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products; 
+        this.filteredProducts = this.products;
+      },
+      error: err => this.erroMessage = err
+    });
+   
   }
- onRatingClicked(message: string): void {
-  this.pageTitle = 'Product List: ' + message;
+  ngOnDestroy() {
+    this.sub.unsubscribe();
 
- }
+  }
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 }
